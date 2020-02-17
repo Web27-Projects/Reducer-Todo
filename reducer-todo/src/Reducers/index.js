47393fe6c0c3e50a2a228e84906e7d73c2f63cca
Miley Wright <dishaliekwright@yahoo.com>
2020-02-17 16:17:12 -0500

    built out initial state

---
+++ reducer-todo/src/Reducers/index.js
@@ -0,0 +1,9 @@
+// add a reducer file and build out a reducer with a default return. Build your initial State object and export both the initial State and reducer.
+
+export const initialState = {
+    listItem: '',
+    todoList: [],
+    completed: false,
+    id: Date.now()
+}
+
